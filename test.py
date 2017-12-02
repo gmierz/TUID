0fe4c024425b5b50a11d2768a69bcfe734613937
berock212 <brockers212@hotmail.com>
2017-12-02 13:13:11 -0500

    Proof of concept for parallelism

---
+++ test.py
@@ -0,0 +1,25 @@
+from multiprocessing import Pool
+from tidservice import TIDService
+import sql
+import json
+import calendar
+import time
+
+thing = TIDService(conn=sql.Sql("resources/stress.db"))
+
+def f(x):
+    return thing.grab_tids(x,"698d4d2ed8c1")
+
+with open('resources/stressfiles.json', 'r') as fil:
+    files = json.load(fil)
+fil.close()
+
+
+if __name__ == '__main__':
+    files=list(map(lambda x : "/dom/base/"+x,files))
+    start = calendar.timegm(time.gmtime())
+    p = Pool(5)
+    print(len(list(map(f, files))))
+    end = calendar.timegm(time.gmtime())
+    print(end-start)
+    print("end")
